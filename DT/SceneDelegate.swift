cc08c566d77050284e8f8648b2addb45d5570675
Ivan Ermak <ermak.ivan.iv@gmail.com>
2019-10-16 10:26:19 +0300

    no message

--- DT/SceneDelegate.swift
+++ DT/SceneDelegate.swift
@@ -19,11 +19,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ProfileOverviewViewController()
-        /*if let accountId = UserDefaults.standard.object(forKey: "defaultUser") as? Int {
+        let navigation = UINavigationController(rootViewController: ProfileOverviewViewController())
+        window?.rootViewController = navigation
+        if let accountId = UserDefaults.standard.object(forKey: "defaultUser") as? Int {
             let profileVC = ProfileViewController(accountIdentifier: accountId)
             navigation.pushViewController(profileVC, animated: true)
-        } */
+        }
         window?.makeKeyAndVisible()
     }
 
